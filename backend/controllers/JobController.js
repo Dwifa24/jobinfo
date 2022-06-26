764c04753e2d6fd97781389b850fca7924ded296
dwifa24 <dwifamayrizka@gmail.com>
2022-06-26 22:39:25 +0700

    finish

--- backend/controllers/JobController.js
+++ backend/controllers/JobController.js
@@ -1,19 +1,35 @@
 const fetch = require('node-fetch');
 
-class JobController{
-    static async getAllJobs (req, res) {
-            const url = 'http://dev3.dansmultipro.co.id/api/recruitment/positions.json';
-            const option = {
-                'method' : 'GET',
-            };
-        
-            const response = await fetch(url, option)
-                .then(res => res.json())
-                .catch(e => {
-                    res.json(err)
-                });
-            res.json(response)
-        }
+
+class JobController {
+    static async getAllJobs(req, res) {
+        const url = 'http://dev3.dansmultipro.co.id/api/recruitment/positions.json';
+        const option = {
+            'method': 'GET',
+        };
+
+        const response = await fetch(url, option)
+            .then(res => res.json())
+            .catch(e => {
+                res.json(e)
+            });
+        res.json(response)
+    }
+
+    static async getDetailJob(req, res) {
+        const id = req.params.id
+        const url = `http://dev3.dansmultipro.co.id/api/recruitment/positions/32bf67e5-4971-47ce-985c-44b6b3860cdb`;
+        const option = {
+            'method': 'GET',
+        };
+
+        const response = await fetch(url, option)
+            .then(res => res.json())
+            .catch(e => {
+                res.json(e)
+            });
+        res.json(response)
+    }
 }
 
 module.exports = JobController
--- backend/routes/job.js
+++ backend/routes/job.js
@@ -2,19 +2,7 @@ const jobRoute = require('express').Router()
 const JobController = require('../controllers/JobController')
 
 jobRoute.get('/', JobController.getAllJobs)
+jobRoute.get('/info', JobController.getDetailJob)
 
-// jobRoute.get('/', async (req, res) => {
-//     const url = 'http://dev3.dansmultipro.co.id/api/recruitment/positions.json';
-//     const option = {
-//         'method' : 'GET',
-//     };
-
-//     const response = await fetch(url, option)
-//         .then(res => res.json())
-//         .catch(e => {
-//             res.json(err)
-//         });
-//     res.json(response)
-// })
 
 module.exports = jobRoute
